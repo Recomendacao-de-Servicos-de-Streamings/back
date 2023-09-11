431752901138f37844dba6a5e6fca2089161c0c7
Bruno Vilardi Bueno <brunovilardibueno@gmail.com>
2023-09-11 10:37:39 -0300

    function URL deployed

--- iac/iac/iac_stack.py
+++ iac/iac/iac_stack.py
@@ -3,7 +3,9 @@ from aws_cdk import (
     Stack,
     aws_lambda_python_alpha,
     aws_lambda as _lambda,
-    aws_iam as iam
+    aws_iam as iam,
+    aws_ec2 as ec2, aws_ecs as ecs,
+    aws_ecs_patterns as ecs_patterns
     
 )
 from constructs import Construct
@@ -44,8 +46,25 @@ class IacStack(Stack):
             timeout=cdk.Duration.seconds(240),
             architecture=_lambda.Architecture.ARM_64
             )
-        
 
+        func.add_function_url(
+            auth_type=_lambda.FunctionUrlAuthType.NONE
+        )
+        
+        # vpc = ec2.Vpc(self, "VPC-Fargate", max_azs=3)     # default is all AZs in region
+
+        # cluster = ecs.Cluster(self, "RecomendacaoCluster", vpc=vpc)
+
+        # ecs_patterns.ApplicationLoadBalancedFargateService(self, "RecomendacaoService",
+        #     cluster=cluster,            # Required
+        #     cpu=512,                    # Default is 256
+        #     desired_count=6,            # Default is 1
+        #     task_image_options=ecs_patterns.ApplicationLoadBalancedTaskImageOptions(
+        #         image=ecs.ContainerImage.from_registry("558695568462.dkr.ecr.us-east-1.amazonaws.com/cdk-hnb659fds-container-assets-558695568462-us-east-1")),
+        #     memory_limit_mib=2048,      # Default is 512
+        #     public_load_balancer=True)  # Default is True
+
+        
         
         
                                   
